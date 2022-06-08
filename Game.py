b93fc894871f5854456ee9723b85fb5e32179312
Tumi Moeng <39015796+DarkPr0digy@users.noreply.github.com>
2022-06-08 21:01:30 +0200

    [Model] Full Night time structure works

--- Game.py
+++ Game.py
@@ -45,25 +45,25 @@ class Game:
         # Loop to decide actions
         for agent in self.agents:
             if agent.is_alive:
-                if agent.role == Role.Escort:
+                if agent.role == Role.Esc:
                     # Distract someone every night
                     distract_target = self.agents[random.randint(0, self.num_agents - 1)]
-                    while distract_target.role == Role.Esc:
+                    while distract_target.role == Role.Esc or distract_target.is_alive is False:
                         distract_target = self.agents[random.randint(0, self.num_agents - 1)]
                     agent.distract(distract_target)
                     visitations[distract_target.name].append(agent)
 
-                elif agent.role == Role.Lookout:
+                elif agent.role == Role.LO:
                     # Observe a player each night
                     observe_target = self.agents[random.randint(0, self.num_agents - 1)]
-                    while observe_target.role == Role.LO:
+                    while observe_target.role == Role.LO or observe_target.is_alive is False:
                         observe_target = self.agents[random.randint(0, self.num_agents - 1)]
 
                 elif agent.role == Role.Doc:
                     # Heal a player each night
                     heal_target = self.agents[random.randint(0, self.num_agents - 1)]
                     try:
-                        while heal_target.used_self_heal:
+                        while heal_target.used_self_heal or heal_target.is_alive is False:
                             heal_target = self.agents[random.randint(0, self.num_agents - 1)]
                     except AttributeError:
                         pass
@@ -79,31 +79,40 @@ class Game:
                 elif agent.role == Role.GF:
                     # Kill someone every night - MVP
                     kill_target = self.agents[random.randint(0, self.num_agents - 1)]
-                    while kill_target.role == Role.GF:
+                    while kill_target.role == Role.GF or kill_target.is_alive is False:
                         kill_target = self.agents[random.randint(0, self.num_agents - 1)]
 
-                    if distract_target.role != Role.GF:
+                    if distract_target is None:
                         agent.kill(kill_target)
                         visitations[kill_target.name].append(agent)
+                    else:
+                        if distract_target.role != Role.GF:
+                            agent.kill(kill_target)
+                            visitations[kill_target.name].append(agent)
+
 
                 # Skip for now
                 elif agent.role == Role.May:
                     pass
-                elif agent.role == Role.Vigilante:
+                elif agent.role == Role.Vig:
                     pass
                 else:
                     print("[Error] Something has gone very wrong.")
 
-            #Vet and Doc cant be distracted
+        print("[Night] Visitations: "+str(visitations))
+        print("[Night] Distract Target: "+str(distract_target))
+        print("[Night] Heal Target: "+str(heal_target))
+        print("[Night] Observe Target: "+str(observe_target))
+        print("[Night] Kill Target: "+str(kill_target))
 
         # Loop to execute actions
         for agent in self.agents:
             if agent.is_alive:
-                if agent.role == Role.Escort:
+                if agent.role == Role.Esc:
                     # Distract someone every night
                     pass
 
-                elif agent.role == Role.Lookout:
+                elif agent.role == Role.LO:
                     # Observe a player each night
                     agent.observe(observe_target,
                                   len(visitations[observe_target.name]) > 0,
@@ -144,7 +153,7 @@ class Game:
 
         for agents in self.agents:
             if agents.is_alive:
-                if agents.role == Role.Mafia:
+                if agents.role == Role.GF:
                     num_mafia_alive += 1
                 else:
                     num_agents_alive += 1
@@ -157,3 +166,18 @@ class Game:
 
 if __name__ == "__main__":
     game = Game(5)
+    while not game._check_win():
+        print("==================\nDay Time\n==================")
+        for agents in game.agents:
+            print(agents.name + ": " + str(agents.role) + " `is alive` is" + str(agents.is_alive))
+
+        game.night_routine()
+        print("==================\nNight Time\n==================")
+
+        for agents in game.agents:
+            print(agents.name + ": " + str(agents.role) + " `is alive` is" + str(agents.is_alive))
+
+    for agents in game.agents:
+        if not agents.is_alive:
+            will, _, _ = agents.get_will()
+            print(will)
